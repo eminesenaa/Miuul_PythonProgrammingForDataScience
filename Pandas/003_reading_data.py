212542fc6a92db8f41331a0de2b21c55c938629a
queciaa <114770554+queciaa@users.noreply.github.com>
2023-09-23 21:24:12 +0300

    ㅤ ㅤ

---
+++ Pandas/003_reading_data.py
@@ -0,0 +1,10 @@
+
+# Pandas can read many proprietary file formats, this study examines a .csv file.
+
+import pandas as pd
+
+df = pd.read_csv("Pandas\Documents for Reading Data\Advertising.csv")
+
+df.head()
+
+# By hovering over the 'pd' and clicking ctrl, we can access the commands to read other files.
