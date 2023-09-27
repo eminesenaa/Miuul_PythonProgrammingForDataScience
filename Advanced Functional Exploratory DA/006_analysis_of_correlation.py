f3842062684735f0b24606d51379209afed5644d
queciaa <114770554+queciaa@users.noreply.github.com>
2023-09-27 18:11:42 +0300

    ㅤ ㅤ

---
+++ Advanced Functional Exploratory DA/006_analysis_of_correlation.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+df = pd.read_csv("datasets/breast_cancer.csv")
+df = df.iloc[:, 1:-1]
+df.head()
+
+num_cols = [col for col in df.columns if df[col].dtype in [int, float]]
+
+corr = df[num_cols].corr()
+
+sns.set(rc={'figure.figsize': (12, 12)})
+sns.heatmap(corr, cmap="RdBu")
+plt.show()
+
+
+# Deleting Highly Correlated Variables
+
+cor_matrix = df.corr().abs()
+
+
+#           0         1         2         3
+# 0  1.000000  0.117570  0.871754  0.817941
+# 1  0.117570  1.000000  0.428440  0.366126
+# 2  0.871754  0.428440  1.000000  0.962865
+# 3  0.817941  0.366126  0.962865  1.000000
+
+
+#     0        1         2         3
+# 0 NaN  0.11757  0.871754  0.817941
+# 1 NaN      NaN  0.428440  0.366126
+# 2 NaN      NaN       NaN  0.962865
+# 3 NaN      NaN       NaN       NaN
+
+
+upper_triangle_matrix = cor_matrix.where(np.triu(np.ones(cor_matrix.shape), k=1).astype(bool))
+drop_list = [col for col in upper_triangle_matrix.columns if any(upper_triangle_matrix[col]>0.90) ]
+cor_matrix[drop_list]
+df.drop(drop_list, axis=1)
+
+def high_correlated_cols(dataframe, plot=False, corr_th=0.90):
+    corr = dataframe.corr()
+    cor_matrix = corr.abs()
+    upper_triangle_matrix = cor_matrix.where(np.triu(np.ones(cor_matrix.shape), k=1).astype(bool))
+    drop_list = [col for col in upper_triangle_matrix.columns if any(upper_triangle_matrix[col] > corr_th)]
+    if plot:
+        import seaborn as sns
+        import matplotlib.pyplot as plt
+        sns.set(rc={'figure.figsize': (15, 15)})
+        sns.heatmap(corr, cmap="RdBu")
+        plt.show()
+    return drop_list
+
+
+high_correlated_cols(df)
+drop_list = high_correlated_cols(df, plot=True)
+df.drop(drop_list, axis=1)
+high_correlated_cols(df.drop(drop_list, axis=1), plot=True)
